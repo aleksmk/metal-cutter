f904d61e6f41b58817d4a353dfde00db59d5da42
Aleks <aleksandar.lazarov1@gmail.com>
2015-09-04 13:09:45 +0200

    Added the global OUTPUT assignments

--- defs.h
+++ defs.h
@@ -12,4 +12,16 @@
 
 unsigned long angularPosition;
 
+// Global OUTPUTs definitions {0 .. 7}
+#define LSAW    0   // Left saw
+#define RSAW    1   // Right saw
+#define HCLAMP  2   // Horizontal clamps
+#define VCLAMP  3   // Vertical clamps
+#define L45     4   // Left 45 degrees valve
+#define L90     5   // Left 90 degrees valve
+#define R45     6   // Right 45 degrees valve
+#define R90     7   // Right 90 degrees valve
+
+// Global INPUTs definitions {0 .. 15}
+
 #endif
